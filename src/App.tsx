417502e4a7fe67273593ce40aca56e7f5fb85531
Reinaldo Landaeta <144262402+REyyyy43@users.noreply.github.com>
2025-08-12 23:15:54 -0400

    Eliminar archivos grandes no deseados

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { FaShareAlt, FaBullhorn, FaCamera, FaBars, FaTimes } from "react-icons/fa";
+import {
+  FaShareAlt,
+  FaBullhorn,
+  FaCamera,
+  FaBars,
+  FaTimes,
+} from "react-icons/fa";
 
 export default function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -9,30 +15,21 @@ export default function App() {
   const navLinkClass =
     "relative px-2 py-1 before:absolute before:bottom-0 before:left-0 before:w-full before:h-[2px] before:bg-[#D29BFD] before:origin-left before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100";
 
-  // Aquí agrego el estado para videos y hover
-  const [videoVisibleIndex, setVideoVisibleIndex] = useState(null);
-  const [hoverIndex, setHoverIndex] = useState(null);
-
   const projects = [
     {
       src: "/assets/portafolio1.jpg",
       role: "Diseño UI/UX y branding",
-      video: "/assets/video1.mp4",
+      link: "https://drive.google.com/drive/folders/129JSYPzpAN48iNn_kbGrNhUEix4S0PhV",
     },
     {
       src: "/assets/portafolio2.jpg",
       role: "Gestión de redes sociales",
-      video: "/assets/video2.mp4",
+      link: "https://drive.google.com/drive/folders/129JSYPzpAN48iNn_kbGrNhUEix4S0PhV",
     },
     {
       src: "/assets/portafolio3.jpg",
       role: "Campañas publicitarias digitales",
-      video: "/assets/video3.mp4",
-    },
-    {
-      src: "/assets/portafolio7.jpg",
-      role: "Consultoría en branding personal",
-      video: "/assets/video4.mp4",
+      link: "https://drive.google.com/drive/folders/129JSYPzpAN48iNn_kbGrNhUEix4S0PhV",
     },
   ];
 
@@ -55,7 +52,9 @@ export default function App() {
           {/* Menú */}
           <nav
             className={`fixed top-[64px] left-0 right-0 bg-[#0A0A0A] md:bg-transparent md:static md:flex md:space-x-6 text-sm font-medium transition-transform duration-300 ease-in-out z-50 ${
-              menuOpen ? "translate-y-0" : "-translate-y-[200%] md:translate-y-0"
+              menuOpen
+                ? "translate-y-0"
+                : "-translate-y-[200%] md:translate-y-0"
             } md:translate-y-0`}
           >
             <a
@@ -129,11 +128,36 @@ export default function App() {
       {/* ABOUT */}
       <section id="about" className="py-16 px-6 bg-gray-950">
         <div className="container mx-auto grid md:grid-cols-2 gap-10 items-center">
-          <img
-            src="/assets/profile.jpg"
-            alt="Sobre mí"
-            className="rounded-lg shadow-lg w-full md:w-2/3 lg:w-1/2 mx-auto"
-          />
+          {/* Collage de imágenes */}
+          <div className="grid grid-cols-3 grid-rows-3 gap-2 w-full md:w-2/3 lg:w-1/2 mx-auto max-h-[380px]">
+            <img
+              src="/assets/profile.jpg"
+              alt="Collage 1"
+              className="col-span-2 row-span-2 object-cover w-full h-full shadow-lg"
+            />
+            <img
+              src="/assets/twouble.jpg"
+              alt="Collage 2"
+              className="object-cover w-full h-full shadow-lg"
+            />
+            <img
+              src="/assets/rutax.jpg"
+              alt="Collage 3"
+              className="object-cover w-full h-full shadow-lg"
+            />
+            <img
+              src="/assets/market4.jpg"
+              alt="Collage 4"
+              className="col-span-1 object-cover w-full h-full shadow-lg"
+            />
+            <img
+              src="/assets/playita.jpg"
+              alt="Collage 5"
+              className="object-cover w-full h-full shadow-lg"
+            />
+          </div>
+
+          {/* Texto */}
           <div>
             <h3 className="text-3xl font-bold mb-4 text-[#D29BFD]">Sobre mí</h3>
             <p className="text-gray-600 mb-4 text-xl">
@@ -156,7 +180,7 @@ export default function App() {
             Lo que puedo hacer por ti
           </h3>
           <div className="grid md:grid-cols-3 gap-8">
-            <div className="bg-neutral-900 shadow-lg rounded-lg p-6 text-center hover:shadow-xl transition">
+            <div className="bg-gray-900 shadow-lg rounded-lg p-6 text-center hover:shadow-xl transition">
               <FaShareAlt className="text-[#D29BFD] text-6xl mx-auto mb-4" />
               <h4 className="font-bold text-gray-500 text-xl mb-2">
                 Gestión de Redes
@@ -166,7 +190,7 @@ export default function App() {
                 comunidad.
               </p>
             </div>
-            <div className="bg-neutral-900 shadow-lg rounded-lg p-6 text-center hover:shadow-xl transition">
+            <div className="bg-gray-900 shadow-lg rounded-lg p-6 text-center hover:shadow-xl transition">
               <FaBullhorn className="text-[#D29BFD] text-6xl mx-auto mb-4" />
               <h4 className="font-bold text-gray-500 text-xl mb-2">
                 Publicidad Digital
@@ -175,7 +199,7 @@ export default function App() {
                 Campañas efectivas, Instagram y Tiktok.
               </p>
             </div>
-            <div className="bg-neutral-900 shadow-lg rounded-lg p-6 text-center hover:shadow-xl transition">
+            <div className="bg-gray-900 shadow-lg rounded-lg p-6 text-center hover:shadow-xl transition">
               <FaCamera className="text-[#D29BFD] text-6xl mx-auto mb-4" />
               <h4 className="font-bold text-gray-500 text-xl mb-2">
                 Creación de Contenido
@@ -196,51 +220,35 @@ export default function App() {
             Mis Proyectos
           </h3>
 
-          {projects.map(({ src, role, video }, index) => (
+          {projects.map(({ src, role, link }, index) => (
             <div
               key={index}
               className={`grid md:grid-cols-2 gap-8 items-center mb-12 ${
                 index % 2 === 1 ? "md:flex-row-reverse" : ""
               }`}
             >
-              <div
-                className="rounded-lg shadow-lg overflow-hidden flex items-center justify-center h-[250px] relative"
-                onMouseEnter={() => setHoverIndex(index)}
-                onMouseLeave={() => setHoverIndex(null)}
-              >
+              <div className="rounded-lg shadow-lg overflow-hidden flex items-center justify-center h-[250px] relative">
                 <img
                   src={src}
                   alt={`Proyecto ${index + 1}`}
                   className="max-h-full max-w-full object-contain"
                 />
-                {hoverIndex === index && (
-                  <button
-                    onClick={() =>
-                      setVideoVisibleIndex(
-                        videoVisibleIndex === index ? null : index
-                      )
-                    }
-                    className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-[#D29BFD] text-white px-4 py-2 rounded hover:bg-[#bb8ff7] transition"
-                  >
-                    {videoVisibleIndex === index ? "Ocultar Video" : "Ver Video"}
-                  </button>
-                )}
               </div>
 
               <div className="text-white">
-                <h4 className="text-2xl font-bold mb-3">Proyecto {index + 1}</h4>
+                <h4 className="text-2xl font-bold mb-3">
+                  Proyecto {index + 1}
+                </h4>
                 <p className="text-gray-300 text-lg">{role}</p>
-
-                {videoVisibleIndex === index && (
-                  <video
-                    src={video}
-                    width="100%"
-                    controls
-                    className="mt-4 rounded-lg shadow-lg"
-                  >
-                    Tu navegador no soporta video HTML5.
-                  </video>
-                )}
+                <a
+                  href={link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="relative inline-block mt-4 bg-gray-900 text-white px-4 py-2 overflow-hidden group"
+                >
+                  <span className="relative z-10">Ver más</span>
+                  <span className="absolute bottom-0 left-0 w-full h-[2px] bg-[#D29BFD] origin-left scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span>
+                </a>
               </div>
             </div>
           ))}
@@ -248,10 +256,7 @@ export default function App() {
       </section>
 
       {/* FOOTER */}
-      <footer
-        id="contact"
-        className="bg-[#0A0A0A] text-white py-6 text-center"
-      >
+      <footer id="contact" className="bg-[#0A0A0A] text-white py-6 text-center">
         <p className="text-sm">
           © {new Date().getFullYear()} María Fernanda Ortiz Duno - Marketing
           Digital & Creación de Contenido.
